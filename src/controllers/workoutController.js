0156475a3831f846059d632ce1a00260c2b3aa38
Edwin <edwintorres0115@hotmail.com>
2024-07-17 18:56:16 -0500

    workout service conectado al controlador ya funcional

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+const workoutService = require('../services/workoutService')
+
 const getAllWorkouts = (req, res) => {
+    const getAllWorkouts = workoutService.getAllWorkouts();
     res.send('Get all workouts');
 };
 
 const getOneWorkout = (req, res) => {
+    const getOneWorkout = workoutService.getOneWorkout(req.params.workoutId);
     res.send(`Get workout ${req.params.workoutId}`);
 };
 
 const createNewWorkout = (req, res) => {
+    const createNewWorkout = workoutService.createNewWorkout(req.params.workoutId);
     res.send(`Create workout ${req.params.workoutId}`);
 };
 
 const updateOneWorkout = (req, res) => {
+    const updateOneWorkout = workoutService.updateOneWorkout(req.params.workoutId);
     res.send(`Update workout ${req.params.workoutId}`);
 };
 
 const deleteOneWorkout = (req, res) => {
+    deleteOneWorkout = workoutService.deleteOneWorkout(req.params.workoutId);
     res.send(`Delete workout ${req.params.workoutId}`);
 };
 
