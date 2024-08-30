a9658ddfe7b6b1a20a6bca8129c4cc09868c4cd3
EdTowers1 <edwintorres0115@hotmail.com>
2024-08-30 16:40:33 -0500

    apicache agregado a routes

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const workoutController = require('../../controllers/workoutController');
+const express = require("express");
+const apicache = require("apicache");
+const workoutController = require("../../controllers/workoutController");
 
-const recordController = require('../../controllers/recordController');
+const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
+const cache = apicache.middleware;
 
 router
-    .get('/', workoutController.getAllWorkouts)
-    .get('/:workoutId', workoutController.getOneWorkout)
-    .get("/:workoutId/records", recordController.getRecordForWorkout)
-    .post('/', workoutController.createNewWorkout)
-    .patch("/:workoutId", workoutController.updateOneWorkout)
-    .delete('/:workoutId', workoutController.deleteOneWorkout);
+  .get("/", cache("2 minutes"), workoutController.getAllWorkouts)
+  .get("/", workoutController.getAllWorkouts)
+  .get("/:workoutId", workoutController.getOneWorkout)
+  .get("/:workoutId/records", recordController.getRecordForWorkout)
+  .post("/", workoutController.createNewWorkout)
+  .patch("/:workoutId", workoutController.updateOneWorkout)
+  .delete("/:workoutId", workoutController.deleteOneWorkout);
 
 module.exports = router;
