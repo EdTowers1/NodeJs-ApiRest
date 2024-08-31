bf43d12a2e29ca55d8292de7be3ba6ac7f6ea5fe
Edwin <edwintorres0115@hotmail.com>
2024-08-30 21:45:25 -0500

    alto uso de RAM por apicache

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const apicache = require("apicache");
+// const apicache = require("apicache");
 const workoutController = require("../../controllers/workoutController");
 
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
-const cache = apicache.middleware;
+// const cache = apicache.middleware;
 
 router
-  .get("/", cache("2 minutes"), workoutController.getAllWorkouts)
+  // .get("/", cache("2 minutes"), workoutController.getAllWorkouts)
   .get("/", workoutController.getAllWorkouts)
   .get("/:workoutId", workoutController.getOneWorkout)
   .get("/:workoutId/records", recordController.getRecordForWorkout)
