1285bc08afc8803e1fd05584963b3df822b9bfc2
Edwin <edwintorres0115@hotmail.com>
2024-09-04 23:33:15 -0500

    correciones de rutas en swaggerJS

--- src/v1/swagger.js
+++ src/v1/swagger.js
@@ -4,7 +4,7 @@ const swaggerUi = require("swagger-ui-express");
 const options = {
     definition: {
         openapi: "3.0.0",
-        info: { tittle: "Crossfit NodeJs API", version: "1.0.0" },
+        info: { title: "Crossfit NodeJs API", version: "1.0.0" },
     },
     apis: ["./src/v1/routes/workoutRoutes.js", "./src/db/Workout.js"],
 };
@@ -14,9 +14,9 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options);
 
 const swaggerDocs = (app, port) => {
-    app.use('api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-    app.get('api/v1/docs.json', (req, res) => {
+    app.get('/api/v1/docs.json', (req, res) => {
         res.setHeader("Content-Type", "application/json");
         res.send(swaggerSpec);
     });
