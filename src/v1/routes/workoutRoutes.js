40290616a817dd768aa0b99e8e428e80d4d97b9b
EdTowers1 <edwintorres0115@hotmail.com>
2024-09-17 16:34:31 -0500

    doc finalizada a falta de ajustes en las responses no documentadas

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -140,8 +140,82 @@ router.get("/:workoutId/records", recordController.getRecordForWorkout);
  */
 router.post("/", workoutController.createNewWorkout);
 
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   patch:
+ *    summary: Actualizar un workout existente
+ *    tags:
+ *       - Workouts
+ *    description: Actualizar un workout existente mediante su ID
+ *    parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of a workout
+ *    requestBody:
+ *      description: campos a actualizar en el workout
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/Workout"
+ *          example:
+ *             name: "Heavy DT"
+ *             mode: "5 Rounds For Time"
+ *             equipment: ["barbell", "rope"]
+ *             exercises: [
+ *               "12 deadlifts",
+ *               "9 hang power cleans",
+ *               "6 push jerks"
+ *             ]
+ *             trainerTips: [
+ *               "Break the exercises into sets if needed",
+ *               "Stay consistent with your pace"
+ *             ]
+ *    responses:
+ *       200:
+ *         description: Workout actualizado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Workout'
+ *       400:
+ *         description: Solicitud incorrecta o datos inválidos
+ *       404:
+ *         description: No se encontró el workout con el ID especificado
+ *       500:
+ *         description: Error interno del servidor
+ * 
+*/
 router.patch("/:workoutId", workoutController.updateOneWorkout);
 
+/**
+ *  @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   delete:
+ *    summary: Eliminar un workout
+ *    tags:
+ *       - Workouts
+ *    description: Eliminar un workout mediante su ID
+ *    parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of a workout
+*    responses:
+ *      200:
+ *        description: OK
+ *        
+ *      404:
+ *        description: Workout not found
+ *      500:
+ *        description: Server error
+ */
 router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
 module.exports = router;
